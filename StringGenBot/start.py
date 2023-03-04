ee203f9b40bcd55379fda575d1eca57ef9a2e4f3
Papaa012 <125028688+Papaa012@users.noreply.github.com>
2023-03-04 15:27:22 +0530

    Update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -9,14 +9,16 @@ def filter(cmd: str):
 
 @Client.on_message(filter("start"))
 async def start(bot: Client, msg: Message):
-    me2 = (await bot.get_me()).mention
-    await bot.send_message(
+    alt = await bot.get_me()
+    me2 = alt.mention
+    await bot.send_photo(
         chat_id=msg.chat.id,
-        text=f"""Hᴇʏ✨🥀 {msg.from_user.mention},
+         photo="https://telegra.ph/file/e71b24bf65a37ecbf5a22.jpg",
+         caption=f"""Hᴇʏ✨🥀 {msg.from_user.mention},
 
 Tʜɪs ɪs {me2},
-sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
-
+A sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
+ᴘᴏᴡᴇʀᴇᴅ ʙʏ ✨🥀 : [𝙄𝙍𝙊](https://t.me/iro_bot_support) !""",
 Mᴀᴅᴇ ᴡɪᴛʜ ❣️ ʙʏ : [𝙋𝙄𝙆𝘼𝘾𝙃𝙐](tg://user?id={OWNER_ID}) !""",
         reply_markup=InlineKeyboardMarkup(
             [
@@ -27,3 +29,8 @@ Mᴀᴅᴇ ᴡɪᴛʜ ❣️ ʙʏ : [𝙋𝙄𝙆𝘼𝘾𝙃𝙐](tg://user?id=
                     InlineKeyboardButton("❣️ 𝙎𝙐𝙋𝙋𝙊𝙍𝙏 ❣️", url="https://t.me/iro_x_support"),
                     InlineKeyboardButton("🥀 𝘾𝙃𝘼𝙉𝙉𝙀𝙇 🥀", url="https://t.me/iro_bot_support"),
                     InlineKeyboardButton("✨ 𝙋𝙄𝙆𝘼𝘾𝙃𝙐 ✨", user_id=OWNER_ID)
+
+                ]
+            ]
+        ),
+    )
