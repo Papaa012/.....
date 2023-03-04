cad44611c3ad02ccb006a3746d3241326da418cf
Papaa012 <125028688+Papaa012@users.noreply.github.com>
2023-03-04 15:07:40 +0530

    Update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -12,22 +12,18 @@ async def start(bot: Client, msg: Message):
     me2 = (await bot.get_me()).mention
     await bot.send_message(
         chat_id=msg.chat.id,
-        text=f"""Hᴇʏ {msg.from_user.mention},
+        text=f"""Hᴇʏ✨🥀 {msg.from_user.mention},
 
 Tʜɪs ɪs {me2},
-Aɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
+sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
 
-Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦](tg://user?id={OWNER_ID}) !""",
+Mᴀᴅᴇ ᴡɪᴛʜ ❣️ ʙʏ : [𝙋𝙄𝙆𝘼𝘾𝙃𝙐](tg://user?id={OWNER_ID}) !""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(text="🙄 ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ 🙄", callback_data="generate")
+                    InlineKeyboardButton(text="✨ ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ ❣️", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton("❣️ sᴏᴜʀᴄᴇ ❣️", url="https://github.com/AnonymousX1025/StringGenBot"),
-                    InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=OWNER_ID)
-                ]
-            ]
-        ),
-        disable_web_page_preview=True,
-    )
+                    InlineKeyboardButton("❣️ 𝙎𝙐𝙋𝙋𝙊𝙍𝙏 ❣️", url="https://t.me/iro_x_support"),
+                    InlineKeyboardButton("🥀 𝘾𝙃𝘼𝙉𝙉𝙀𝙇 🥀", url="https://t.me/iro_bot_support"),
+                    InlineKeyboardButton("✨ 𝙋𝙄𝙆𝘼𝘾𝙃𝙐 ✨", user_id=OWNER_ID)
